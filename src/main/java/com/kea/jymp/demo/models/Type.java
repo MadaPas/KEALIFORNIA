ee30cbbbe644e43585adba0963b5adef5da5d4a7
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2019-09-03 13:15:57 +0200

    models

--- src/main/java/com/kea/jymp/demo/models/Type.java
+++ src/main/java/com/kea/jymp/demo/models/Type.java
@@ -15,4 +15,38 @@ public class Type {
         this.capacity = capacity;
         this.price = price;
     }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
