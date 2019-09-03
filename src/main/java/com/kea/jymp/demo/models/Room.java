c1f95140650e6becddbf0f581ee1afdad33750be
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2019-09-03 22:31:41 +0200

    models

--- src/main/java/com/kea/jymp/demo/models/Room.java
+++ src/main/java/com/kea/jymp/demo/models/Room.java
@@ -3,5 +3,39 @@ package com.kea.jymp.demo.models;
 public class Room {
 
     private int id;
+    private boolean isFree;
+    private int typeId;
+
+    public Room() {
+    }
+
+    public Room(boolean isFree, int typeId) {
+        this.isFree = isFree;
+        this.typeId = typeId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isFree() {
+        return isFree;
+    }
+
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
 
 }
