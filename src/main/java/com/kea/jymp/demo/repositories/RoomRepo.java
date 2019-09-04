9d194e86c3f608e27077c3641d2e63080b5ff743
plamistef <plamstef@abv.bg>
2019-09-04 11:33:48 +0200

    find all rooms

--- src/main/java/com/kea/jymp/demo/repositories/RoomRepo.java
+++ src/main/java/com/kea/jymp/demo/repositories/RoomRepo.java
@@ -4,6 +4,9 @@ import com.kea.jymp.demo.models.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.simpleflatmapper.jdbc.spring.JdbcTemplateMapperFactory;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,15 +17,33 @@ public class RoomRepo {
     @Autowired
     private JdbcTemplate jdbc;
 
+    // Maps one-to-many JOIN query into Bookings and List<Tickets> in each Booking
+    private final ResultSetExtractor<List<Room>> resultSetExtractor =
+            JdbcTemplateMapperFactory
+                    .newInstance()
+                    .addKeys("id")
+                    .newResultSetExtractor(Room.class);
+
+
     public List<Room> findAll() {
-        String sql = "SELECT * FROM room";
-        List<Room> rooms = jdbc.query(sql, new BeanPropertyRowMapper<>(Room.class));
+        String sql = getJoinedQuery();
+        List<Room> rooms = jdbc.query(sql, resultSetExtractor);
+        System.out.println(rooms);
         return rooms;
     }
     public Room findOne(int id) {
         String sql = "SELECT * FROM room WHERE id = "+id;
         return jdbc.queryForObject(sql, new BeanPropertyRowMapper<>(Room.class));
 
+    }
+
+    private String getJoinedQuery() {
+        String query =
+                "SELECT r.id as id, r.isFree," +
+                        "rt.id as roomType_id, rt.name as roomType_name, rt.capacity as roomType_capacity, rt.price as roomType_price" +
+                        " FROM room r" +
+                        " JOIN roomType rt ON rt.id = r.type_id";
+        return query;
     }
 
 
