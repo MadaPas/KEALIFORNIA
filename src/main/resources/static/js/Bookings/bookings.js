591fcf28fdd46c04f3023e17e18d87cc6b182493
jfbn <johan.bagger@hotmail.com>
2019-09-05 19:32:53 +0200

    adds one ajax call

--- src/main/resources/static/js/Bookings/bookings.js
+++ src/main/resources/static/js/Bookings/bookings.js
@@ -1,5 +1,15 @@
 $(function(){
 
+let booking;
+
+    booking = {
+        "roomId": 1,
+        "customerId": 1,
+        "startDate": "2019-02-02",
+        "endDate": "2019-02-07",
+        "noOfGuests": 3
+    }
+
 
     //
     $.ajax({
@@ -9,7 +19,7 @@ $(function(){
         data: JSON.stringify(booking),
         contentType: 'application/json; charset=utf-8'
     })
-        .done
+        .done(console.log("ajax sent"));
 
 
 });
