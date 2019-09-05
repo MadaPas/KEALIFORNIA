80f580f42aaaeca435fe46cceb0a1ee903d38868
yewonblairseo@gmail.com <yewonblairseo@gmail.com>
2019-09-05 10:48:18 +0200

    merge attempt

--- src/main/java/com/kea/jymp/demo/controllers/CustomerController.java
+++ src/main/java/com/kea/jymp/demo/controllers/CustomerController.java
@@ -1,26 +1,51 @@
 package com.kea.jymp.demo.controllers;
 
+
 import com.kea.jymp.demo.models.Customer;
 import com.kea.jymp.demo.repositories.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CustomerController {
 
     @Autowired
     CustomerRepo customerRepo;
 
-    // Get all bookings
+    // Get all customers
     @GetMapping("/api/customers")
     @ResponseBody
     public List<Customer> findAll() {
         List<Customer> customers = customerRepo.findAll();
         return customers;
+    }
+
+    // Get one customer
+    @GetMapping("/api/customers/{id}")
+    @ResponseBody
+    public Customer findOne(@PathVariable int id) {
+        return customerRepo.findOne(id);
+    }
+
+    // Delete one customer
+    @DeleteMapping("/api/customers/{id}")
+    @ResponseBody
+    public void deleteOne(@PathVariable int id) {
+        customerRepo.deleteOne(id);
+    }
+
+    // Update one customer
+    @PutMapping("/api/customers/{id}")
+    @ResponseBody
+    public void updateOne(@PathVariable int id, @RequestBody Customer updatedObject){
+
+        System.out.println("dd");
+        System.out.println(updatedObject);
+        customerRepo.updateOne(id, updatedObject);
+
     }
 
 }
